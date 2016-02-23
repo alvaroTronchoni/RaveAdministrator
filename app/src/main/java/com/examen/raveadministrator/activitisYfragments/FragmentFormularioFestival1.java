8237c1ebcc10fc12773caa3ef23be683b9130642
alvaro <alvaroroto@hotmail.com>
2016-02-23 12:12:33 +0100

    pantalla mis festivales con boton añadir festival

--- app/src/main/java/com/examen/raveadministrator/activitisYfragments/FragmentFormularioFestival1.java
+++ app/src/main/java/com/examen/raveadministrator/activitisYfragments/FragmentFormularioFestival1.java
@@ -65,6 +65,27 @@ public class FragmentFormularioFestival1 extends Fragment {
     private OnFragmentInteractionListener mListener;
     private RVClasificacionesAdapter adapter;
     private static final String PREFERENCIAS = "Preferencias";
+    private String claveApi;
+
+    private EditText edit_nombre;
+    private EditText edit_genero;
+    private EditText edit_ubicacion;
+    private EditText edit_latitud;
+    private EditText edit_longitud;
+    private EditText edit_descripcion;
+    private EditText edit_face_url;
+    private EditText edit_web_url;
+    private EditText edit_logo_url;
+    private EditText edit_back_url;
+    private EditText edit_entradas_url;
+    private EditText edit_fecha_inicio;
+    private EditText edit_fecha_fin;
+    private EditText edit_asistentes;
+    private EditText edit_ediciones;
+    private EditText edit_instagram_user;
+
+    private Button btn_cancel;
+    private Button btn_acept;
 
 
     /**
@@ -99,6 +120,9 @@ public class FragmentFormularioFestival1 extends Fragment {
 
             ButterKnife.inject((AppCompatActivity)getActivity());
         }
+
+        SharedPreferences prefs = getActivity().getSharedPreferences(PREFERENCIAS, Context.MODE_PRIVATE);
+        claveApi = prefs.getString("claveApi", "");
 
 
        //((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("FESTIVALES");
@@ -185,5 +209,249 @@ public class FragmentFormularioFestival1 extends Fragment {
         LinearLayoutManager llm = new LinearLayoutManager(getActivity());
         rv.setLayoutManager(llm);
         rv.setAdapter(adapter);*/
+    }
+
+    private void recogerVariables(View v){
+        edit_nombre = (EditText) v.findViewById(R.id.input_nombreFestival);
+        edit_genero = (EditText) v.findViewById(R.id.input_generoFestival);
+        edit_ubicacion = (EditText) v.findViewById(R.id.input_ubicacionFestival);
+        edit_latitud = (EditText) v.findViewById(R.id.input_latitudFestival);
+        edit_longitud = (EditText) v.findViewById(R.id.input_longitudFestival);
+        edit_descripcion = (EditText) v.findViewById(R.id.input_descripcionFestival);
+        edit_face_url = (EditText) v.findViewById(R.id.input_facebookUrl);
+        edit_web_url = (EditText) v.findViewById(R.id.input_webUrl);
+        edit_logo_url = (EditText) v.findViewById(R.id.input_logoUrl);
+        edit_back_url = (EditText) v.findViewById(R.id.input_backUrl);
+        edit_instagram_user = (EditText) v.findViewById(R.id.input_instagramUser);
+        edit_entradas_url = (EditText) v.findViewById(R.id.input_entradasUrl);
+        edit_fecha_inicio = (EditText) v.findViewById(R.id.input_fechaInicio);
+        edit_fecha_fin = (EditText) v.findViewById(R.id.input_fechaFin);
+        edit_asistentes = (EditText) v.findViewById(R.id.input_asistentesFestival);
+        edit_ediciones = (EditText) v.findViewById(R.id.input_edicionesFestival);
+        btn_acept = (Button) v.findViewById(R.id.btnAceptar);
+        btn_cancel = (Button) v.findViewById(R.id.btnAtras);
+
+        btn_acept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(puedo()){
+
+                }
+            }
+        });
+
+    }
+
+    public String[] respuestas(){
+        String respuesta[] = new String[16];
+        respuesta[0] = edit_nombre.getText().toString();
+        respuesta[1] = edit_genero.getText().toString();
+        respuesta[2] = edit_ubicacion.getText().toString();
+        respuesta[3] = edit_latitud.getText().toString();
+        respuesta[4] = edit_longitud.getText().toString();
+        respuesta[5] = edit_descripcion.getText().toString();
+        respuesta[6] = edit_face_url.getText().toString();
+        respuesta[7] = edit_web_url.getText().toString();
+        respuesta[8] = edit_logo_url.getText().toString();
+        respuesta[9] = edit_back_url.getText().toString();
+        respuesta[10] = edit_instagram_user.getText().toString();
+        respuesta[11] = edit_entradas_url.getText().toString();
+        respuesta[12] = edit_fecha_inicio.getText().toString();
+        respuesta[13] = edit_fecha_fin.getText().toString();
+        respuesta[14] = edit_asistentes.getText().toString();
+        respuesta[15] = edit_ediciones.getText().toString();
+
+        return respuesta;
+    }
+
+    public boolean puedo(){
+        boolean valid = true;
+
+
+        if (edit_nombre.getText().toString().isEmpty()) {
+            edit_nombre.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_nombre.setError(null);
+        }
+        if (edit_genero.getText().toString().isEmpty()) {
+            edit_genero.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_genero.setError(null);
+        }
+        if (edit_ubicacion.getText().toString().isEmpty()) {
+            edit_ubicacion.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_ubicacion.setError(null);
+        }
+        if (edit_latitud.getText().toString().isEmpty()) {
+            edit_latitud.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_latitud.setError(null);
+        }
+        if (edit_longitud.getText().toString().isEmpty()) {
+            edit_longitud.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_longitud.setError(null);
+        }
+        if (edit_descripcion.getText().toString().isEmpty()) {
+            edit_descripcion.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_descripcion.setError(null);
+        }
+        if (edit_face_url.getText().toString().isEmpty()) {
+            edit_face_url.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_face_url.setError(null);
+        }
+        if (edit_web_url.getText().toString().isEmpty()) {
+            edit_web_url.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_web_url.setError(null);
+        }
+        if (edit_logo_url.getText().toString().isEmpty()) {
+            edit_logo_url.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_logo_url.setError(null);
+        }
+        if (edit_back_url.getText().toString().isEmpty()) {
+            edit_back_url.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_back_url.setError(null);
+        }
+        if (edit_instagram_user.getText().toString().isEmpty()) {
+            edit_instagram_user.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_instagram_user.setError(null);
+        }
+        if (edit_entradas_url.getText().toString().isEmpty()) {
+            edit_entradas_url.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_entradas_url.setError(null);
+        }
+        if (edit_fecha_inicio.getText().toString().isEmpty()) {
+            edit_fecha_inicio.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_fecha_inicio.setError(null);
+        }
+        if (edit_fecha_fin.getText().toString().isEmpty()) {
+            edit_fecha_fin.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_fecha_fin.setError(null);
+        }
+        if (edit_asistentes.getText().toString().isEmpty()) {
+            edit_asistentes.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_asistentes.setError(null);
+        }
+        if (edit_ediciones.getText().toString().isEmpty()) {
+            edit_ediciones.setError("introduce los datos requeridos");
+            valid = false;
+        } else {
+            edit_ediciones.setError(null);
+        }
+
+        return valid;
+    }
+
+    public class MeterEvento extends AsyncTask<String[], Void, String> {
+
+        @Override
+        protected String doInBackground(String[]... params) {
+
+            StringBuilder favouriteDataJsonStr = new StringBuilder();
+
+            String http = "http://ubuntu.westeurope.cloudapp.azure.com/ApiRAVE/v1/festivales";
+
+            HttpURLConnection urlConnection=null;
+            try {
+
+
+                URL url = new URL(http);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setUseCaches(false);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestProperty("Authorization", claveApi);
+                urlConnection.connect();
+
+                //Create JSONObject here
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("nombreFestival", "Mad Cool Festival");
+                jsonParam.put("idAdministrador", "8");
+                jsonParam.put("generoFestival", "Indie, Rock");
+                jsonParam.put("ubicacionFestival", "Madrid");
+                jsonParam.put("latitudFestival", "40.4378698");
+                jsonParam.put("longitudFestival", "-3.8196207");
+                jsonParam.put("descripcionFestival", "El Mad Cool Festival se celebrará en un recinto ubicado en la Caja Mágica de Madrid del 16 al 18 de junio de 2016.");
+                jsonParam.put("facebookUrl", "https://www.facebook.com/madcoolfestival/");
+                jsonParam.put("webUrl", "http://madcoolfestival.es/");
+                jsonParam.put("logoUrl", "http://static.guiaocio.com/var/guiadelocio.com/storage/images/conciertos/madrid/madrid/mad-cool-festival-2016/27885014-10-esl-ES/mad-cool-festival-2016.jpg");
+                jsonParam.put("backUrl", "http://5www.ecestaticos.com/imagestatic/clipping/587/acd/85c/587acd85c091f684137c7cd35a4dbe9d/bienvenidos-al-mad-cool-el-nuevo-macrofestival-musical-de-madrid.jpg?mtime=1453892094");
+                jsonParam.put("instagramUser", "madcoolfestival");
+                jsonParam.put("entradasUrl", "http://madcoolfestival.es/entradas/");
+                jsonParam.put("fechaInicio", "2016-06-16");
+                jsonParam.put("fechaFin", "2016-06-18");
+                jsonParam.put("asistentesFestival", "40.000");
+                jsonParam.put("edicionesFestival", "1");
+
+                OutputStreamWriter out = new OutputStreamWriter(urlConnection.getOutputStream());
+                out.write(jsonParam.toString());
+                out.close();
+
+                int HttpResult = urlConnection.getResponseCode();
+                if(HttpResult == HttpURLConnection.HTTP_OK){
+                    BufferedReader br = new BufferedReader(new InputStreamReader(
+                            urlConnection.getInputStream(),"utf-8"));
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        favouriteDataJsonStr.append(line + "\n");
+                    }
+                    br.close();
+
+                }else{
+                    System.out.println(urlConnection.getResponseMessage());
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }finally{
+                if(urlConnection!=null)
+                    urlConnection.disconnect();
+            }
+
+            return null;//getFavouriteDataFromJson(favouriteDataJsonStr.toString());
+
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            if(result!=null){
+
+                //Toast.makeText(getApplicationContext(), "Imagen guardada correctamente", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
